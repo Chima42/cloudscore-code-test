0c9d296448c1fd69bb030dd63cba04c0fd39698b
Chima Nwosu <chimannwosu@gmail.com>
2025-02-24 10:11:15 +0000

    Fix: Alignment on cards on mobile

--- src/Components/Drawer/DrawerDetail.tsx
+++ src/Components/Drawer/DrawerDetail.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from "react";
-import style from "./style.module.scss";
+import style from "./styles.module.scss";
 
 function DrawerDetailWrapper({ children }: PropsWithChildren) {
   return <div className={style.wrapper}> {children} </div>;
--- src/Components/Pill/Pill.tsx
+++ src/Components/Pill/Pill.tsx
@@ -1,10 +1,11 @@
-import style from "./style.module.scss";
+import style from "./styles.module.scss";
 import { IPill } from "../../Interfaces/IPill";
 
 function Pill({ onTrack, label, type }: IPill) {
   function classNames(classes: string[]) {
     return classes.join(" ");
   }
+
   return type === "impact" ? (
     <span className={classNames([style.pill, style.impact])}>{label}</span>
   ) : (
